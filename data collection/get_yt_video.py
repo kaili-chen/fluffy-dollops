7e9d227d6139c7d08534dc3aa48ac662786bf7a9
KaiLi Chen <kailichen@KaiLis-MacBook-Pro.local>
2020-08-31 21:38:33 +0800

    added optional argument for cmd useage (-v/--video)

--- data collection/get_yt_video.py
+++ data collection/get_yt_video.py
@@ -1,3 +1,12 @@
+'''
+Contains methods to assist in extracting lyrics from genius.com.
+
+Functions:
+    get_album_tracks(string) --> list object
+    get_lyrics(string) --> dictionary object
+
+'''
+
 import argparse
 import os
 import sys
@@ -5,6 +14,7 @@ import csv
 from pytube import YouTube
 
 # https://pypi.org/project/pytube3/
+
 # UTILITY FUNCTIONS
 def is_yt_url(url):
     '''
@@ -56,19 +66,24 @@ def read_file(path, skip=None):
     return None
 
 
-
-
-def download_video(url, max_tries = 20):
+# FUNCTIONS
+def download_video(url, max_tries = 20, video_only = False):
     '''
     Downloads video from youtube url
 
     Parameters:
         url (string): youtube url
         max_tries (int) [default = 20]: number of times to try downloading link before aborting
+        video_only (bool) [default = False]:
+            if True: might get video with no audio but higher resolution
+            if False / None: always get mp4 (if any)
     Returns:
         if successful: file path of downloaded video
         if unsuccessful: None
     '''
+
+    # TODO: add path_out
+    # TODO: deal with seperate audio and video files
 
     passed = False
     try_count = 0
@@ -76,8 +91,12 @@ def download_video(url, max_tries = 20):
         # odd error occurs at times, refer to https://github.com/nficano/pytube/issues/393 -hence the while loop
         try:
             yt = YouTube(url)
-            # dl = yt.streams.filter(only_video=True).order_by('resolution')[-1].download()
-            dl = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1].download()
+
+            print("downloading...")
+            if video_only:
+                dl = yt.streams.filter(only_video=True).order_by('resolution')[-1].download()
+            else:
+                dl = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1].download()
             print(dl)
 
             passed = True
@@ -97,8 +116,11 @@ if __name__ == "__main__":
     # positional arguments
     ap.add_argument('input', help='input file or youtube url')
 
-    args = vars(ap.parse_args())
+    # optional arguments
+    ap.add_argument('-v', '--video', type=bool, help='indicate whether to download video only')
 
+    args = vars(ap.parse_args())
+    # print(args)
     # checks if input is a file path
     input = args['input']
     is_file = os.path.isfile(input)
@@ -106,9 +128,9 @@ if __name__ == "__main__":
         print("File found")
         lines = read_file(input, skip=is_yt_url)
         for line in lines:
-            download_video(line)
+            download_video(line, video_only=args['video'])
     elif is_yt_url(input):
         print("URL found")
-        download_video(input)
+        download_video(input, video_only=args['video'])
     else:
         print("no file/url found")
