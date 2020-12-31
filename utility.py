a49bc0cdfe0dbf30b7c2da0476b45049edb15cad
kaili <26805525+kaili-chen@users.noreply.github.com>
2020-12-31 16:44:35 +0800

    general utility file

---
+++ utility.py
@@ -0,0 +1,34 @@
+'''
+Contains ugeneral tility functions
+
+Functions
+-------
+- save_json(data, filename) : saves data (dict) to a json file
+'''
+
+import os
+import json
+
+### GENERAL UTILITY FUNCTIONS
+def save_json(data, filename):
+    '''
+    Saves data to a json file.
+
+    Parameters:
+        data (dict): data to save to json file
+        filename (string): filename to save json file as (saves to current dir by default)
+
+    Returns:
+        path (string): absolute path to json file
+    '''
+
+    # TODO: use path_out instead of filename instead?
+
+    # checks if file already has json extension
+    if ".json" not in filename:
+        filename = "{}.json".format(filename)
+
+    with open(filename, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4, sort_keys=True)
+
+    return os.path.abspath(filename)
