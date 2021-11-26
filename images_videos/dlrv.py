bdb4072a030eb877080287ae8221fecb4e12945b
kaili <26805525+kaili-chen@users.noreply.github.com>
2021-11-26 23:42:29 +0800

    add dlrv script
    dlrv.y script to download reddit video

---
+++ images_videos/dlrv.py
@@ -0,0 +1,70 @@
+"""
+script to download video from a reddit post using url
+"""
+
+import urllib.request
+import json
+import subprocess
+import os
+import argparse
+
+def merge_video_audio(video_path, audio_path, output_filename):
+    # ffmpeg -i <video> -i <audio> -c:v copy -c:a aac output.mp4
+    subprocess.call('ffmpeg -i {} -i {} -c:v copy -c:a aac {}'.format(video_path, audio_path, output_filename), shell=True)
+
+    # cleanup temp files
+    if os.path.isfile(video_path): os.remove(video_path)
+    if os.path.isfile(audio_path): os.remove(audio_path)
+
+def main(url, output_path='output.mp4', use_file=False):
+    '''
+    Parameters
+    --------------
+    - url   (str):  reddit post url / json file path
+    - output_path   (str)   [default = 'output.mp4']: output mp4 path
+    - use_file  (bool)  [default = False]:  if true, reads url as json file path instead of reddit url
+
+    Returns
+    --------------
+    None
+    '''
+    if use_file:
+        print('reading file')
+        # res = json.load(open('reddit_download/reddit_req.json', 'r'))
+        res = json.load(open(url, 'r'))
+    else:
+        print('reading url')
+        url = url[:-1]+'.json'
+        res_obj = urllib.request.urlopen(url)
+        res = json.load(res_obj)
+
+    post_item = res[0]
+    post_data = post_item['data']
+    video_info = post_data['children'][0]['data']['secure_media']
+    # pprint.pprint(video_info)
+    video_url = video_info['reddit_video']['fallback_url']
+    # print(video_url)
+    audio_url = video_url[:video_url.find('DASH_')+len('DASH_')]+'audio.mp4?source=fallback'
+
+    print('downloading video...')
+    urllib.request.urlretrieve(video_url, filename='output_video.mp4')
+    print('downloading audio...')
+    urllib.request.urlretrieve(audio_url, filename='output_audio.mp4')
+
+    merge_video_audio('output_video.mp4', 'output_audio.mp4', output_path)
+
+if __name__ == '__main__':
+    
+    # initialise parser
+    parser = argparse.ArgumentParser(description=__doc__)
+    # add positional arguments
+    parser.add_argument("url")
+    # add optional arguments
+    parser.add_argument('-o', '--output', required=False, help="file path to save resulting mp4 file", type=str)
+    parser.add_argument('-f', '--usefile', required=False, help="if true, reads url as json file path", type=bool)
+
+    # read arguments
+    args = parser.parse_args()
+    print(args)
+    # run main method
+    # main(args.url, output_path=args.output)
